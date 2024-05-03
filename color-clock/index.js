a69be5068176643585251d2688c6cd8f6029dccb
William Ndirangu <wwagura0@gmail.com>
2024-05-03 11:58:24 +0300

    Add

--- color-clock/index.js
+++ color-clock/index.js
@@ -1,6 +1,6 @@
 //No need to do any work here.
 //If package.json and the proper modules are installed in the correct location, this should display correctly in the browser.
-import format from "./node_modules/date-fns/esm/format/index.js";
+import format from "date-fns";
 
 function displayTime() {
   const clock = document.querySelector("#clock");
